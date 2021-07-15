c754d5a417dd6713526ffc84adb843c1a93a4d06
Nisal-Sashmitha <it20081034@my.sliit.lk>
2021-07-15 18:22:33 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import ClientManager from './component/ClientManager'
 
 
 function App() {
   return (
     <div className="App">
       <ClientManager/>
-      
+      {/** hello world*/}
     </div>
   );
 }
