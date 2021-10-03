4638af035e6ee4da78e8e05f8ad2852e587933a8
lkh-krishen <86856408+lkh-krishen@users.noreply.github.com>
2021-10-03 18:30:05 +0530

    Added Report

--- src/component/ClientManager/DisplayPayments.js
+++ src/component/ClientManager/DisplayPayments.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../../firebase";
 import { Link } from "react-router-dom";
-import { Table, Button, ButtonGroup } from "react-bootstrap";
+import { Form, Table, Button, ButtonGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function DisplayPayments(props) {
   const [payments, setPayments] = useState([]);
+  const [clientNames, setClientNames] = useState([]);
+  const [projectSearch, setProjectSearch] = useState("");
+  const [projectNames, setProjectNames] = useState([]);
   const db = firebase.firestore();
   const [editingPayment, setEditingPayment] = useState(props);
+  const [clientSearch, setClientSearch] = useState("");
 
   useEffect(() => {
     db.collection("payments").onSnapshot((snapshot) => {
@@ -18,6 +22,19 @@ function DisplayPayments(props) {
 
       console.log(arr);
       setPayments(arr);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    db.collection("clients").onSnapshot((snapshot) => {
+      const arr = snapshot.docs.map((doc) => doc.data().clientName);
+      setClientNames(arr);
+    });
+  }, [db]);
+  useEffect(() => {
+    db.collection("Con_Project").onSnapshot((snapshot) => {
+      const arr = snapshot.docs.map((doc) => doc.data().Title);
+      setProjectNames(arr);
     });
   }, [db]);
 
@@ -39,7 +56,33 @@ function DisplayPayments(props) {
   }
 
   return (
-    <div>
+    <div style={{ paddingRight: "20px", paddingLeft: "20px" }}>
+      <Form.Control
+        style={{ width: "400px" }}
+        as="select"
+        onChange={(event) => {
+          setClientSearch(event.target.value);
+        }}
+      >
+        <option value="">Report for client: </option>
+        {clientNames.map((clientName) => (
+          <option value={clientName}>{clientName}</option>
+        ))}
+      </Form.Control>
+      <br />
+      <Form.Control
+        style={{ width: "400px" }}
+        as="select"
+        onChange={(event) => {
+          setProjectSearch(event.target.value);
+        }}
+      >
+        <option value="">Report for client's project: </option>
+        {projectNames.map((projectName) => (
+          <option value={projectName}>{projectName}</option>
+        ))}
+      </Form.Control>
+      <br />
       <Link to="/adminPannel/ClientManager/AddPayment">
         <Button style={{ borderRadius: "10px 10px 0 0" }} variant="primary">
           Add New Payment
@@ -58,53 +101,65 @@ function DisplayPayments(props) {
           </tr>
         </thead>
         <tbody>
-          {payments.map((payment) => (
-            <tr>
-              <td style={{ display: "none" }}>{payment.ID}</td>
-              <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-                {payment.data.clientName}
-              </td>
-              <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-                {payment.data.projectName}
-              </td>
-              <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-                {payment.data.date}
-              </td>
-              <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-                {payment.data.amount}
-              </td>
-              <td style={{ textAlign: "center" }}>
-                <ButtonGroup>
-                  <Link to="/adminPannel/ClientManager/EditPayment">
+          {payments
+            .filter((payment) => {
+              if (clientSearch == "") {
+                return payment;
+              } else if (payment.data.clientName == clientSearch) {
+                if (projectSearch == "") {
+                  return payment;
+                } else if (payment.data.projectName == projectSearch) {
+                  return payment;
+                }
+              }
+            })
+            .map((payment) => (
+              <tr>
+                <td style={{ display: "none" }}>{payment.ID}</td>
+                <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                  {payment.data.clientName}
+                </td>
+                <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                  {payment.data.projectName}
+                </td>
+                <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                  {payment.data.date}
+                </td>
+                <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                  {payment.data.amount}
+                </td>
+                <td style={{ textAlign: "center" }}>
+                  <ButtonGroup>
+                    <Link to="/adminPannel/ClientManager/EditPayment">
+                      <Button
+                        style={{ borderRadius: "5px 0 0 5px" }}
+                        variant="warning"
+                        onClick={() => {
+                          editPayment(payment.ID);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                    </Link>
                     <Button
-                      style={{ borderRadius: "5px 0 0 5px" }}
-                      variant="warning"
+                      style={{ borderRadius: "0 5px 5px 0" }}
+                      variant="danger"
                       onClick={() => {
-                        editPayment(payment.ID);
+                        if (
+                          window.confirm(
+                            "Are you sure you want to delete this payment's details? This action is irreversible!"
+                          )
+                        ) {
+                          deletePayment(payment.ID);
+                        }
                       }}
                     >
-                      Edit
+                      Delete
                     </Button>
-                  </Link>
-                  <Button
-                    style={{ borderRadius: "0 5px 5px 0" }}
-                    variant="danger"
-                    onClick={() => {
-                      if (
-                        window.confirm(
-                          "Are you sure you want to delete this payment's details? This action is irreversible!"
-                        )
-                      ) {
-                        deletePayment(payment.ID);
-                      }
-                    }}
-                  >
-                    Delete
-                  </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-          ))}
+                  </ButtonGroup>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
     </div>
